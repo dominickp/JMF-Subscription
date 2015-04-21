e810f201783eb9d1fd1bd7ab3cc90cf99b679283
Dominick Peluso <peluso.dominick@gmail.com>
2015-04-21 09:32:27 -0400

    Update server to get request body and log it

--- JmfSubscriptionServer.js
+++ JmfSubscriptionServer.js
@@ -1,25 +1,36 @@
-//Lets require/import the HTTP module
+// Require modules
 var http = require('http');
 var winston = require('winston');
 
+// Prepare log file
 winston.add(winston.transports.File, { filename: 'requests.log' });
 
-//Lets define a port we want to listen to
+// Lets define a port we want to listen to
 const PORT=9090;
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
+// We need a function which handles requests and send response
+function handleRequest(request, response){ 
+    
+    var body = '';
+     
+    request.on('data', function (data) {
+        body += data;
+        console.log("Partial body: " + body);
+    });
+    request.on('end', function () {
+        // Log the body of the request
+        winston.info('Incoming Request', { url: request.url, body: body });
+    });
+    
+    // Send a response
     response.end('It Works!! Path Hit: ' + request.url);
-
-    // Log it
-    winston.info(request);
 }
 
-//Create a server
+// Create a server
 var server = http.createServer(handleRequest);
 
-//Lets start our server
+// Lets start our server
 server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
+    // Callback triggered when server is successfully listening
     console.log("Server listening on: http://localhost:%s", PORT);
 });
