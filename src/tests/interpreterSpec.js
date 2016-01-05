32e3412fd1c70184c6fcdb5c7b29b9c04f3ba09e
Dominick Peluso <peluso.dominick@gmail.com>
2016-01-05 11:00:25 -0500

    Cleanup

--- src/tests/interpreterSpec.js
+++ src/tests/interpreterSpec.js
@@ -11,15 +11,15 @@ describe("interpreter", function() {
             {DeviceID: 'Press1', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press1", SignalType: "SignalStatus",ProductionCounter: 35092505, createdAt:new Date('December 17, 2015 03:20:00')},
             {DeviceID: 'Press1', DeviceStatus: "Running",StatusDetails: "Indigo: Printing", SignalID: "SJDFSpy_Detail-Press1", SignalType: "SignalStatus",ProductionCounter: 35098505, createdAt:new Date('December 17, 2015 03:25:00')},
             {DeviceID: 'Press1', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press1", SignalType: "SignalStatus",ProductionCounter: 35098505, createdAt:new Date('December 17, 2015 03:30:00')},
+            {DeviceID: 'Press2', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press2", SignalType: "SignalStatus",ProductionCounter: 25092505, createdAt:new Date('December 17, 2015 03:20:00')},
+            {DeviceID: 'Press2', DeviceStatus: "Running",StatusDetails: "Indigo: Printing", SignalID: "SJDFSpy_Detail-Press2", SignalType: "SignalStatus",ProductionCounter: 25098505, createdAt:new Date('December 17, 2015 03:25:00')},
+            {DeviceID: 'Press2', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press2", SignalType: "SignalStatus",ProductionCounter: 25098505, createdAt:new Date('December 17, 2015 03:30:00')},
             {DeviceID: 'Press1', DeviceStatus: "Running",StatusDetails: "Indigo: Printing", SignalID: "SJDFSpy_Detail-Press1", SignalType: "SignalStatus",ProductionCounter: 35100505, createdAt:new Date('December 17, 2015 03:35:00')},
             {DeviceID: 'Press1', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press1", SignalType: "SignalStatus",ProductionCounter: 35100605, createdAt:new Date('December 17, 2015 03:40:00')},
             {DeviceID: 'Press1', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press1", SignalType: "SignalStatus",ProductionCounter: 35100605, createdAt:new Date('December 17, 2015 03:45:00')},
             {DeviceID: 'Press1', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press1", SignalType: "SignalStatus",ProductionCounter: 35100605, createdAt:new Date('December 17, 2015 03:50:00')},
             {DeviceID: 'Press1', DeviceStatus: "Running",StatusDetails: "Indigo: Printing", SignalID: "SJDFSpy_Detail-Press1", SignalType: "SignalStatus",ProductionCounter: 35100605, createdAt:new Date('December 17, 2015 03:55:00')},
             {DeviceID: 'Press1', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press1", SignalType: "SignalStatus",ProductionCounter: 35100705, createdAt:new Date('December 17, 2015 04:00:00')},
-            {DeviceID: 'Press2', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press2", SignalType: "SignalStatus",ProductionCounter: 25092505, createdAt:new Date('December 17, 2015 03:20:00')},
-            {DeviceID: 'Press2', DeviceStatus: "Running",StatusDetails: "Indigo: Printing", SignalID: "SJDFSpy_Detail-Press2", SignalType: "SignalStatus",ProductionCounter: 25098505, createdAt:new Date('December 17, 2015 03:25:00')},
-            {DeviceID: 'Press2', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press2", SignalType: "SignalStatus",ProductionCounter: 25098505, createdAt:new Date('December 17, 2015 03:30:00')},
             {DeviceID: 'Press2', DeviceStatus: "Running",StatusDetails: "Indigo: Printing", SignalID: "SJDFSpy_Detail-Press2", SignalType: "SignalStatus",ProductionCounter: 25100505, createdAt:new Date('December 17, 2015 03:35:00')},
             {DeviceID: 'Press2', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press2", SignalType: "SignalStatus",ProductionCounter: 25100605, createdAt:new Date('December 17, 2015 03:40:00')},
             {DeviceID: 'Press2', DeviceStatus: "Idle",StatusDetails: "Indigo: Ready",       SignalID: "SJDFSpy_Detail-Press2", SignalType: "SignalStatus",ProductionCounter: 25100605, createdAt:new Date('December 17, 2015 03:45:00')},
@@ -110,12 +110,9 @@ describe("interpreter", function() {
                     expect(range.start).toBe(timeTable[index][0]);
                     expect(range.end).toBe(timeTable[index][1]);
                 }
-
                 // Start should be less than end
                 expect(range.start).toBeLessThan(range.end);
             });
         });
-
     });
-
 });
